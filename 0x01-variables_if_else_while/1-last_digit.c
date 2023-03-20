05761b21b95f6a144cda72e8073753480c1a11b7
moalfy7 <125768940+moalfy7@users.noreply.github.com>
2023-03-21 01:56:32 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
- *Return: Always 0 (Success)
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
@@ -13,9 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	printf("Last digit of %i is %i and is ", n, (n % 10));
-
 	if ((n % 10) == 0)
 	{
 		printf("0\n");
