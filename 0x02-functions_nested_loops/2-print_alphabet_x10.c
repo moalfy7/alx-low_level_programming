925ed57ed87da54c8ef851e7a67f60748d2d28d1
moalfy7 <125768940+moalfy7@users.noreply.github.com>
2023-03-21 19:51:29 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,16 +10,14 @@ void print_alphabet_x10(void)
 {
 	char alpha;
 	int i;
-	
 	for (i = 1; i <= 10; i++)
 	{
 		alpha = 'a';
 		while (alpha <= 'z') 
-		{
+	{
 			putchar(alpha);
 			alpha++;
 		}
 		putchar('\n');
 	}
-	
-}
+	}
