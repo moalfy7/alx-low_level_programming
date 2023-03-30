b38f05774ab05d38cd1d7a649de832183bcf2e3f
moalfy7 <125768940+moalfy7@users.noreply.github.com>
2023-03-31 01:22:01 +0200

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,19 +10,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, len1 = 0, len2 = 0;
+	int i;
+	int j;
 
-    while (dest[len1] != '\0' || src[len2] != '\0')
-    {
-        if (dest[len1] != '\0')
-            len1++;
-        if (src[len2] != '\0')
-            len2++;
-    }
-    for (i = 0; i < len2 && i < n; i++)
-    {
-        dest[len1] = src[i];
-        len1++;
-    }
-    return (dest);
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
