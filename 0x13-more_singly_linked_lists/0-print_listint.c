9507b21b069a76458adc24a781560ba6b62c574e
moalfy7 <125768940+moalfy7@users.noreply.github.com>
2023-05-02 16:48:36 +0300

    Update 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		_putchar(h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
